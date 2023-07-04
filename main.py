f1794513b17f6efe712d696b3ab2a0e04f5d8d02
Rizky Rahman <rizkpython87@gmail.com>
2023-07-04 11:15:07 +0800

    fix xpath and click ()

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 driver.get(website)
+
+all_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
+all_matches_button.click()

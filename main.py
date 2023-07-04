30ea12f0a63d4d8ce491b68388a9f08694d2acd5
Rizky Rahman <rizkpython87@gmail.com>
2023-07-04 12:47:03 +0800

    fix loop for matches

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ all_matches_button.click()
 
 matches = driver.find_elements(By.TAG_NAME, 'tr')
 
+date = []
+home_team = []
+score = []
+away_team = []
+
 for match in matches:
-    print(match.text)
+    date.append(match.find_element(By.XPATH, './td[1]').text)
+    home = match.find_element(By.XPATH, './td[2]').text
+    home_team.append(home)
+    print(home)
+    score.append(match.find_element(By.XPATH, './td[3]').text)
+    away_team.append(match.find_element(By.XPATH, './td[4]').text)
+
 

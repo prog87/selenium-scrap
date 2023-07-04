c1eacf4d78b4028ab95d5158a75ca7c015daf05b
Rizky Rahman <rizkpython87@gmail.com>
2023-07-04 12:26:59 +0800

    fix data matches

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ driver.get(website)
 
 all_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
 all_matches_button.click()
+
+matches = driver.find_elements(By.TAG_NAME, 'tr')
+
+for match in matches:
+    print(match.text)
+
